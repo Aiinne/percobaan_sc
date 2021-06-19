295dbdb59dea4499303b2f746136c905111f4d3a
Qisyana <79745902+qisyana@users.noreply.github.com>
2021-06-19 23:13:26 +0700

    Create time120m.js

---
+++ time120m.js
@@ -0,0 +1,365 @@
+case 'test':
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 7200000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 7140000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 7080000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 7020000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 6960000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 6900000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 6840000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 6780000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 6720000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 6660000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 6600000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 6540000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 6480000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 6420000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 6360000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 6300000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 6240000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 6180000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 6120000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 6060000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 6000000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 5940000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 5880000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 5820000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 5760000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 5700000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 5640000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 5580000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 5520000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 5460000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 5400000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 5340000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 5280000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 5220000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 5160000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 5100000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 5040000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 4980000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 4920000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 4860000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 4800000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 4740000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 4680000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 4620000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 4560000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 4500000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 4440000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 4380000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 4320000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 4260000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 4200000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 4140000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 4080000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 4020000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 3960000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 3900000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 3840000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 3780000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 3720000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 3660000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 3600000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 3540000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 3480000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 3420000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 3360000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 3300000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 3240000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 3180000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 3120000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 3060000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 3000000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 2940000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 2880000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 2820000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 2760000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 2700000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 2640000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 2580000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 2520000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 2460000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 2400000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 2340000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 2280000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 2220000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 2160000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 2100000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 2040000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 1980000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 1920000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 1860000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 1800000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 1740000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 1680000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 1620000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 1560000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 1500000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 1440000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 1380000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 1320000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 1260000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 1200000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 1140000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 1080000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 1020000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 960000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 900000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 840000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 780000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 720000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 660000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 600000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 540000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 480000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 420000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 360000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 300000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 240000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 180000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 120000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 60000)
+					setTimeout( () => {
+					client.sendMessage(from, 'Active', text)
+					}, 0)
+			break
