512c2b5c278fa47c97c1d00dc885c76404556689
Qisyana <79745902+qisyana@users.noreply.github.com>
2021-07-27 20:54:16 +0700

    Update sound_aine.js

--- percobaan_sc2/sound_aine.js
+++ percobaan_sc2/sound_aine.js
@@ -4,9 +4,9 @@ fetch('https://raw.githubusercontent.com/qisyana/scrape/main/aine.txt')
   .then(res => res.text())
   .then(txt => arrs = txt.split('\n'))
 let handler = async (m, { conn }) => {
-  let ainee = arrs[Math.floor(Math.random() * arrs.length)]
-  if (!ainee) throw false
-  await conn.sendFile(m.chat, ainee, ainee, null, m, true, { mimetype: 'audio/mp4' })
+  let aine = arrs[Math.floor(Math.random() * arrs.length)]
+  if (!aine) throw false
+  await conn.sendFile(m.chat, aine, aine, null, m, true, { mimetype: 'audio/mp4' })
 }
 handler.customPrefix = /^aine$/i
 handler.command = new RegExp
